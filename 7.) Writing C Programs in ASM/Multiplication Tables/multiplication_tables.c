9205f0259e4fd255d0cb7c5cbf71a24e90d7e2a2
Mudassar Islam <demoncious@gmail.com>
2019-10-11 12:48:22 +0500

    Create multiplication_tables.c

---
+++ 7.) Writing C Programs in ASM/Multiplication Tables/multiplication_tables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+  int number;
+  
+  printf("Please Enter a Number to Print the Table of: ");
+  scanf("%d", &number);
+  
+  for(int i = 1; i < 11; i++) {
+    printf("%d x %d = %d", number, i, (number * i));
+  }
+  
+  getch();
+}
